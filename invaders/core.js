9fb8ecc87843d2cb8337c499ad7fc572c758033e
Daniel Cenoz <cuervodaniel@rocketmail.com>
2019-04-12 16:17:53 -0300

    exercice with canvas - for loop

--- invaders/core.js
+++ invaders/core.js
@@ -2,21 +2,22 @@
 //space invaders core
 
 window.document.addEventListener('DOMContentLoaded', function(){
-
+    
     const panel = document.querySelector('#panel');
     //context
     const ctx = canvas.getContext('2d');
 
-    ctx.fillStyle = 'blue';
-    ctx.fillRect(10, 10, 100, 100);
+    let colorArray = ["red","blue","green","lightblue"];
+    let x = 0;
+    let y = 0;
 
-    ctx.fillStyle = 'red';
-    ctx.fillRect(15, 15, 100, 100);
+    for(var i = 0; i < 4; i ++){
+        x = x + 10;
+        y = y + 10;
 
-    ctx.fillStyle = 'green';
-    ctx.fillRect(20, 20, 100, 100);
+        ctx.fillStyle = colorArray[i];
+        ctx.fillRect(x, y, 100, 100);
 
-    ctx.fillStyle = 'lightblue';
-    ctx.fillRect(25, 25, 100, 100);
+    }
 
 });
