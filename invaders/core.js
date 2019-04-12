8bde26421cd2664dbde6543a9740e694d7b7048b
Daniel Cenoz <cuervodaniel@rocketmail.com>
2019-04-12 18:34:09 -0300

    squares only

--- invaders/core.js
+++ invaders/core.js
@@ -7,17 +7,45 @@ window.document.addEventListener('DOMContentLoaded', function(){
     //context
     const ctx = canvas.getContext('2d');
 
-    let colorArray = ["red","blue","green","lightblue"];
-    let x = 0;
-    let y = 0;
+    let colorArray = ["red","blue","green","lightblue"],
+        colorArrayLenght = colorArray.length,
+        x = 0,
+        y = 0,
+        i = 0,
+        sqWidth = 50,
+        sqHight = 50;
 
+    setInterval(function(){
+        
+        ctx.clearRect(x, y, sqHight, sqWidth);
+        x = x + 5;
+        y = x + 5;
+
+
+        ctx.fillStyle = colorArray[i % colorArrayLenght];
+        ctx.fillRect(x, y, sqHight, sqWidth);
+        
+        if ( i == colorArrayLenght){
+            ctx.clearRect(x, y, sqHight, sqWidth);
+            i = 0;
+            x = 0;
+            y = 0;
+        } else {
+            i ++;
+        }
+
+    }, 1000);
+
+    /*
     for(var i = 0; i < 4; i ++){
         x = x + 10;
         y = y + 10;
 
         ctx.fillStyle = colorArray[i];
         ctx.fillRect(x, y, 100, 100);
+        ctx.clearRect(x, y, 100, 100);
 
     }
+    */
 
 });
